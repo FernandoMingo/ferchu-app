4e83026f7a2b1072f11440b0c53feeb7bb969cb9
Ferchu Mingo <ferchumingo@Ferchus-MacBook-Air.local>
2023-09-26 19:24:19 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MyThing from './MyThing'
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <code>Ferchu App</code>
+          <MyThing />
         </p>
         <a
           className="App-link"
@@ -18,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
---
+++ src/MyThing.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+function MyThing() {
+  return (
+    <div className="App">
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default MyThing;
