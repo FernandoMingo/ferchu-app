2d2b091be588d79bc7926583fdd50a499b5a849e
Fernando <mingomorenof@gmail.com>
2023-09-27 15:43:33 +0200

    Fix bugs

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <WebContent />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
